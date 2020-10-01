9fcee9f318efdfa16e61a951d1b11a446e2881cf
Gunashree C <55580701+Guna-777@users.noreply.github.com>
2020-10-01 18:15:10 +0530

    Update home.page.dart

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -3,11 +3,23 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
+  // appbar with title Home
   Widget build(BuildContext context) => new Scaffold(
+    backgroundColor: Color(0xFF284940),
     appBar: new AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
       title: new Text('Home'),
     ),
+   // container with logout option
     body: new Container(
+      decoration: BoxDecoration(
+         color: Colors.white,
+         borderRadius: BorderRadius.only(
+         topLeft: Radius.circular(40.0),
+         topRight: Radius.circular(40.0)
+                ),
+              ),
       margin: new EdgeInsets.only(
         top: 50.0
       ),
